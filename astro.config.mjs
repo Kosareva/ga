8350010dc669702342ede2ceebcae55c712d1929
kosareva <kosareva962@gmail.com>
2024-03-16 20:12:03 +0200

    Add ag

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,15 @@
 import {defineConfig} from 'astro/config';
+import partytown from '@astrojs/partytown';
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://kosareva.github.io",
-  base: "/ga.github.io"
+  base: "/ga.github.io",
+  integrations: [
+    partytown({
+      config: {
+        forward: ['dataLayer.push'],
+      },
+    }),
+  ],
 });
